19e0075b1c2bc297841168e968d073f5d3b435e7
mimilun <mimilun@mail.ru>
2021-11-01 16:05:38 +0300

    feat: add 01

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const fs = require('fs');
+const currentFile = path.join(__dirname, 'text.txt');
+
+let readStream = fs.createReadStream(currentFile, 'utf-8');
+
+readStream.on('data', (chank) => {
+    console.log(currentFile);
+  console.log(chank);
+});
+
