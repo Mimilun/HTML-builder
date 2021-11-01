b8e71b5171ed895042778a18ac40b15c88115d2f
mimilun <mimilun@mail.ru>
2021-11-01 17:12:18 +0300

    fix

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const fs = require('fs');
-const currentFile = path.join(__dirname, 'text.txt');
+const pathFile = path.join(__dirname, 'text.txt');
 
-let readStream = fs.createReadStream(currentFile, 'utf-8');
+let readStream = fs.createReadStream(pathFile, 'utf-8');
 
 readStream.on('data', (chank) => {
   console.log(chank);
