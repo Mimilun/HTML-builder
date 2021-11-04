8d7dca7bde618c4de7a64ee49e462b0d4f2d61e1
mimilun <mimilun@mail.ru>
2021-11-04 18:07:52 +0300

    feat: add 3 task

---
+++ 02-write-file/index.js
@@ -0,0 +1,24 @@
+const readLine = require('readline');
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+
+let writeableStream = fs.createWriteStream(path.join(__dirname, 'test.txt'));
+
+let rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', (line) => {
+  if (line === 'exit'.trim()) {
+    writeableStream.end();
+    rl.close();
+  } else {
+    writeableStream.write(line + os.EOL);
+  }
+});
+
+rl.on('close', () => {
+  console.log('Good byu');
+});
---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs/promises');
+const path = require('path');
+const pathDir = path.join(__dirname);
+
+fs.readdir(pathDir + '/secret-folder', 'utf-8').then((content) => {
+  content.forEach((el) => {
+    fs.stat(pathDir + '/secret-folder/' + el).then((stat) => {
+      if (stat.isFile()) {
+        console.log(`${path.basename(el, path.extname(el))} - ${stat.size / 1024}kb - ${path.extname(el).slice(1)}`);
+      }
+    });
+  });
+});
+
