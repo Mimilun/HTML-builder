1ae1f9acdb4a32bf7eae6e2d2ddfd8bac593c532
mimilun <mimilun@mail.ru>
2021-11-05 13:42:57 +0300

    fix: fix 3 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,10 +1,9 @@
 const fs = require('fs/promises');
 const path = require('path');
-const pathDir = path.join(__dirname);
 
-fs.readdir(pathDir + '/secret-folder', 'utf-8').then((content) => {
+fs.readdir(__dirname + '/secret-folder', 'utf-8').then((content) => {
   content.forEach((el) => {
-    fs.stat(pathDir + '/secret-folder/' + el).then((stat) => {
+    fs.stat(__dirname + '/secret-folder/' + el).then((stat) => {
       if (stat.isFile()) {
         console.log(`${path.basename(el, path.extname(el))} - ${path.extname(el).slice(1)} - ${stat.size / 1024}kb`);
       }
