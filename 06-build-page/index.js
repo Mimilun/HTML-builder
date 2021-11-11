e04e2a6d12e4f27dc5904c1c4d3b8d0cfd2d7f2e
mimilun <mimilun@mail.ru>
2021-11-11 22:53:09 +0300

    fix: update task 6

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,75 +1,90 @@
 const path = require('path');
 const fs = require('fs/promises');
 const fsStream = require('fs');
-
-const pathOutFile = path.join(__dirname, 'project-dist', 'style.css');
-const writeFile = fsStream.createWriteStream(pathOutFile);
+const pathDir = path.join(__dirname, 'project-dist');
 
 // 1
-createDir(path.join(__dirname, 'project-dist'));
+buildHtml();
+
+async function buildHtml() {
+  await fs.rm(pathDir, {recursive: true, force: true});
+  await fs.mkdir(pathDir, {recursive: true});
+  task2();
+  task3();
+  task4();
+}
+
 
 // 2
+async function task2() {
+  fsStream.readFile(path.join(__dirname, 'template.html'), 'utf-8', (err, data) => {
+    let html = data.toString();
+    fs.readdir(path.join(__dirname, 'components')).then((files) =>
+      files.forEach((file) => {
+        let fileName = path.parse(file).name;
+        fs.readFile(path.join(__dirname, 'components', file), 'utf-8').then((data) => {
+          html = html.replace(`{{${fileName}}}`, data);
+          fs.writeFile(path.join(__dirname, 'project-dist', 'index.html'), html);
+        });
+      })
+    );
+  });
+}
 
-fsStream.readFile(path.join(__dirname, 'template.html'), 'utf-8', (err, data) => {
-  let html = data.toString();
-  fs.readdir(path.join(__dirname, 'components')).then((files) =>
-    files.forEach(async (file) => {
-      let fileName = path.parse(file).name;
-      fs.readFile(path.join(__dirname, 'components', file), 'utf-8').then((data) => {
-        html = html.replace(`{{${fileName}}}`, data);
-        fs.writeFile(path.join(__dirname, 'project-dist', 'index.html'), html);
-      });
-    })
-  );
-
-});
 
 // 3
+async function task3() {
+  const styleFile = path.join(__dirname, 'project-dist', 'style.css');
+  const writeFile = fsStream.createWriteStream(styleFile);
 
-fsStream.readdir(path.join(__dirname, 'styles'), (err, content) => {
-  content.forEach((file) => {
-    const currentFile = path.join(__dirname, 'styles', file);
-    if (path.extname(file) === '.css') {
-      fs.stat(currentFile).then((stat) => {
-        if (stat.isFile()) {
-          let readFile = fsStream.createReadStream(path.join(__dirname, 'styles', file), 'utf-8');
-
-          readFile.on('data', (chunk) => {
-            writeFile.write(chunk);
-          });
-        }
-      });
-    }
+  fsStream.readdir(path.join(__dirname, 'styles'), (err, content) => {
+    content.forEach((file) => {
+      const currentFile = path.join(__dirname, 'styles', file);
+      if (path.extname(file) === '.css') {
+        fs.stat(currentFile).then((stat) => {
+          if (stat.isFile()) {
+            let readFile = fsStream.createReadStream(path.join(__dirname, 'styles', file), 'utf-8');
+            readFile.on('data', (chunk) => {
+              writeFile.write(chunk);
+            });
+          }
+        });
+      }
+    });
   });
-});
+}
 
 // 4
-createDir(path.join(__dirname, 'project-dist', 'assets'));
-copyDir(path.join(__dirname, 'assets'), path.join(__dirname, 'project-dist', 'assets'));
+async function task4() {
+  fs.mkdir(path.join(__dirname, 'project-dist', 'assets')).then(() => {
+    copyDir(path.join(__dirname, 'assets'), path.join(__dirname, 'project-dist', 'assets'));
+  });
+}
 
-function copyDir(src, dest) {
-  fs.readdir(src).then((content) => {
-    for (let el of content) {
-      fs.stat(path.join(src, el)).then((status) => {
-        if (status.isDirectory()) {
-          createDir(path.join(dest, el));
-          copyDir(path.join(src, el), path.join(dest, el));
-        } else {
-          fs.copyFile(path.join(src, el), path.join(dest, el)).then(() => {
+async function copyDir(src, dest) {
+  fs.readdir(src)
+    .then((content) => {
+      for (let el of content) {
+        fs.stat(path.join(src, el))
+          .then((status) => {
+            if (status.isDirectory()) {
+              fs.mkdir(path.join(dest, el), {recursive: true})
+                .then(() => {
+                  copyDir(path.join(src, el), path.join(dest, el));
+                });
+            } else {
+              fs.copyFile(path.join(src, el), path.join(dest, el))
+                .then(() => {
+                });
+            }
           });
-        }
-      });
-    }
-  }).catch(() => {
-  });
+      }
+    });
 }
 
 
-function createDir(pathDir) {
-  fs.mkdir(pathDir, {recursive: true}).then(() => {
-  }).catch(() => {
-  });
-}
+
+
 
 
 
